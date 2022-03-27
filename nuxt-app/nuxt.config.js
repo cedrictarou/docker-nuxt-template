c4ce0812c97460d83022acdfc3c7aaff37e9fda7
cedrictaro-g <cedric.tarou@gmail.com>
2022-03-27 11:12:02 +0900

    nuxt vuetify template

--- nuxt-app/nuxt.config.js
+++ nuxt-app/nuxt.config.js
@@ -35,7 +35,9 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    // https://vuetifyjs.com/en/getting-started/installation/#nuxt-install
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
